6e496776822e1bc58cdb5aacb848777175a27b38
Denis Sandalov <deonis78@gmail.com>
2014-07-17 16:03:59 +0700

    Add support of time log

--- src/redmine_helper/report_processor.clj
+++ src/redmine_helper/report_processor.clj
@@ -2,7 +2,7 @@
   (:require [clojure.java.io :as io])
   (:require [clojure.string :as su]))
 
-(def coefficients {"Simple" 0.5, "Normal" 1, "Hard" 1.5, "Very Hard" 3})
+(def coefficients {"Simple" 0.5, "Normal" 1, "Hard" 1.5, "Very Hard" 2})
 
 (defn folder-fn [acc el]
   (let [rec (su/split el #"\t+")
@@ -19,11 +19,20 @@
   (reduce + 0
     (for [[k v] complexities :when (contains? coefficients k)] (* (get coefficients k) (parse-double v)))))
 
+(defn create-log-map [time-log el]
+  (assoc time-log (first el) (parse-double(second el))))
+
+(defn work-time [time-log name]
+  (if-let [time (get time-log name)] time 0))
+
 (defn run-processor []
-(with-open [reader (io/reader "spent_time.txt") w (io/writer "result.csv")]
+(with-open [reader (io/reader "spent_time.txt") w (io/writer "result.csv")
+            time-log-reader (io/reader "time_log.csv")]
   (let [rows (line-seq reader)
-        result (reduce folder-fn [] rows)]
+        result (reduce folder-fn [] rows)
+        time-log (reduce create-log-map {} (csv/read-csv time-log-reader :separator \,))]
     (doseq [line result] (.write w
-      (str "\"" (get line :name) "\";" (format "%.2f" (calculate-complexity (get line :complexity))) "\n"))))))
-
-
+      (format "\"%s\";%.2f;%.2f\n"
+        (:name line)
+        (calculate-complexity (:complexity line))
+        (/ (work-time time-log (:name line)) (parse-double (:total line)))))))))
